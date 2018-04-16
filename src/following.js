25fd470517ea3bbee0d1e5c4e23b34b2deb9e46a
VicoErv <vicoerv@gmail.com>
2018-04-16 11:52:43 +0800

    following refactored es6

--- src/following.js
+++ src/following.js
@@ -1,17 +1,17 @@
-const DataStore = require('nedb');
+import DataStore from "nedb";
 
 const path = __dirname + '/local/db/following';
 
-function Following (db) {
-    this.db = new DataStore({ filename: path, autoload: true});;
-}
+export default class Following {
+    Following(db) {
+        this.db = new DataStore({ filename: path, autoload: true });;
+    }
 
-Following.prototype.byId = function (id, callback) {
-    this.db.find({ userId: id }, callback);
-}
+    byId(id, callback) {
+        this.db.find({ userId: id }, callback);
+    }
 
-Following.prototype.add = function (data, callback) {
-    this.db.insert(data, callback);
+    add(data, callback) {
+        this.db.insert(data, callback);
+    }
 }
-
-module.exports = Following;
